b0305d6d803f1912ff0e3039389f4b410be91dcf
liufugui11 <644712767@qq.com>
2026-07-25 03:11:55 +0800

    feat: add Vue3 App root component

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+// App根组件
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
